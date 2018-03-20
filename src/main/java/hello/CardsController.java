1ab153a41e816ad838f4a9434bcd3269a4a2abf1
kris <kris979@gmail.com>
2018-03-20 21:06:53 +0000

    initial commit

---
+++ src/main/java/hello/CardsController.java
@@ -0,0 +1,23 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@RestController
+@Api
+public class CardsController {
+    
+	@RequestMapping(value = "/cards", method = RequestMethod.GET)
+    public String index() {
+        return "Cards";
+    }
+    
+    
+    //localhost:8080/swagger-ui.html
+//    http://localhost:8080/v2/api-docs
+//    http://localhost:8080/swagger-ui.html#/
+}
---
+++ src/main/java/hello/HelloController.java
@@ -0,0 +1,18 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@Api
+public class HelloController {
+    
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+    
+}
