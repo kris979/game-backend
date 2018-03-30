37d2369769d67a876226d8d11d27d98c33e1cebe
kris <kris979@gmail.com>
2018-03-30 14:43:47 +0100

    reading board size from application.properties file

--- src/main/java/app/ModelConfig.java
+++ src/main/java/app/ModelConfig.java
@@ -1,14 +1,20 @@
 package app;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:/application.properties")
 public class ModelConfig {
+	
+	@Value("${board.size}")
+	private Integer boardSize;
 
     @Bean
     public Integer size() {
-    	return 10;
+    	return boardSize;
     }
     
 }
